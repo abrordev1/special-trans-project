25a1cbf555e0a20f451f64b87ab1ab8ddaac6a40
abrordev1 <abrortajimuratov833@gmail.com>
2023-06-08 11:57:11 +0500

    feat: completed hero section (2023-06-08, 11:56)

--- src/pages/HomePage/sections/HeroSection.vue
+++ src/pages/HomePage/sections/HeroSection.vue
@@ -1,8 +1,31 @@
 <template>
-    <section class="hero h-screen bg bg-right">
+    <section class="hero min-h-screen bg bg-right lg:bg-center flex items-end py-5 md:items-center">
+        <div class="container">
+            <div class="block relative max-w-[750px]">
+                <div class="bg-layer w-full h-full absolute left-0 top-0 bg-[#878787] opacity-50 blur-[100px]"></div>              
+                <h1 class="title relative text-3xl leading-9 text-white font-bold md:text-6xl ">Продажа и доставка нерудных материалов</h1>
+                <p class="subtitle relative text-white md:text-xl leading-5 my-4 lg:mt-8 lg:mb-16 ">
+                    У нас вы можете заказать материалы премиального качества.
+                    Индивидуальный подход и качественная продукция – ключевые преимущества
+                    нашей компании.
+                </p>
+                <TheButton class="relative">Заказать звонок</TheButton>
+            </div>
+        </div>
 
     </section>
 </template>
+
+<script>
+import TheButton from '../../../ui/TheButton.vue';
+
+
+
+export default {
+    components: { TheButton }
+}
+
+</script>
 
 <style lang="scss">
 .hero {
